4ce27571a9f10f63067c274308228555635f2b27
FunkeCoder23 <adt27@zips.uakron.edu>
2019-10-13 14:42:10 -0400

    working on Shell

--- Shell.c
+++ Shell.c
@@ -6,31 +6,53 @@ void main()
   int j;//total string iterator
   char input[80];
   char cmd[80];
-  char msg1[80];
+  char arg1[80];
+  char arg2[80];
   interrupt(0x21,12,10,20,0); //clear screen w/o changing colors
-  interrupt(0x21,0,"Hello, Welcome to ShittyDOS\r\n",0,0);
+  interrupt(0x21,0,"Hello, Welcome to BlackDOS Shell\r\n",0,0);
 
   while(1)
   {
+    for(i=0;i<80;++i)
+    {
+      input[i]='\0';  //clear input buffer
+    }
+
     i=0;
     j=0;
-    cmd[0]="\0";
+    cmd[0]='\0';
+    arg1[0]='0';
+    arg2[0]='0';
+
     interrupt(0x21,0,"^(~(oo)~)^: ",0,0);
     interrupt(0x21,1,input,0,0);   //readString(input)
     while(input[i]!=' '&& input[i]!='\0') //check for space or endline
     {
-      //if(i=5)
-      //{
-      //  cmd[0]='X';
-      //  break;
-      //}
-      cmd[i]=input[i]; //store cmd word
-      i++;
-    //  interrupt(0x21,0,cmd[i-1],0,0);
-    //  j++;  //total string iterator
+      cmd[i]=input[j]; //store cmd word
+      i++;  //inc cmd iter
+      j++;  //inc string iterator
+    }
+    i=0; //reset internal iter
+    while(input[i]!=' '&& input[i]!='\0') //check for space or endline
+    {
+      arg1[i]=input[j]; //store cmd word
+      i++;  //inc cmd iter
+      j++;  //inc string iterator
+    }
+    i=0; //reset internal iter
+    while(input[i]!=' '&& input[i]!='\0') //check for space or endline
+    {
+      arg2[i]=input[j]; //store cmd word
+      i++;  //inc cmd iter
+      j++;  //inc string iterator
     }
     interrupt(0x21,0,cmd,0,0);
     interrupt(0x21,0,"\r\n");
+    interrupt(0x21,0,arg1,0,0);
+    interrupt(0x21,0,"\r\n");
+    interrupt(0x21,0,arg2,0,0);
+    interrupt(0x21,0,"\r\n");
+
     /*
     *input++;
     j=0;
