8638bb9658ce99b271bb1b35f18e20b1c7016360
FunkeCoder23 <adt27@zips.uakron.edu>
2019-10-29 11:50:33 -0400

    Helpfile works, just needs the text!

--- help.c
+++ help.c
@@ -4,26 +4,27 @@ char* readText(char* );
 
 void main()
 {
-  int i;
+  int line=0;
+  int page=0;
   char *c; //continue char
-  /*
-  //static char helpText[0][80]="Nulla vitae diam eget lacus placerat consequat in at dui.\r\n";
-  //static char helpText[1][80]=    "Vivamus vel odio feugiat, elementum mauris vel, dictum mi.\r\n";
-  //static char helpText[2][80]=    "In in sapien vel nisi pretium porttitor nec ut ante.\r\n";
-  //static char helpText[3][80]=    "Etiam volutpat ipsum quis nisi porta dictum.\r\n";
-  */
-  //char *helpText=readText("Aenean tristique est at velit convallis, ut feugiat lectus tincidunt. Donec id lectus eu nulla venenatis placerat non ac elit.  Nullam dignissim urna quis sapien hendrerit, id commodo nisi eleifend.  Donec eu dui vel mauris auctor sodales.  Nullam lacinia enim a eros rhoncus, id scelerisque quam fringilla.  Vivamus aliquam neque quis facilisis dictum.  Etiam id sem eu elit bibendum mattis.  Quisque ut leo ut lacus volutpat vulputate.  Donec sit amet risuspellentesque,consectetur turpis ultrices, elementum mi.  Phasellus in ipsum in augue volutpat auctor id at metus.  Phasellus accumsan metus id efficitur dapibus.  Morbi euismod nulla ut tempor aliquet.  Ut pulvinar nibh molestie posuere euismod.  Nullam consectetur diam tincidunt elit porta vestibulum.  Proin fringilla mi sit amet pretium lacinia.  Pellentesque sit amet odio ut sem maximus tincidunt at a dolor.");
+  char* helpText[80]; //helpText buffer
 
+
+  helpText[0]="Nulla vitae diam eget lacus placerat consequat in at dui.\r\n";
+  helpText[1]=    "Vivamus vel odio feugiat, elementum mauris vel, dictum mi.\r\n";
+  helpText[2]=    "In in sapien vel nisi pretium porttitor nec ut ante.\r\n";
+  helpText[3]=    "Etiam volutpat ipsum quis nisi porta dictum.\r\n";
+  
 
   do
   {
-    for (i=0;i<24;i++)
+    for (line=(0+page*24); line<(24*(page+1)); ++line)
     {
-    //interrupt(0x21,0,helpText+i,0,0);
+      interrupt(0x21,0,helpText[line],0,0);
     }
-
     interrupt(0x21,0,"Press q to quit, any other key to continue.\r\n");
     *c = interrupt(0x16,0,0,0,0); //read in cont char
+    page++;
   }while(*c != 'q');
   interrupt(0x21,5,0,0,0);  //END and return to shell
 }
